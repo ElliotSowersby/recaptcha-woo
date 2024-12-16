e675a2417eaee7103f433543f5b6ca43afcd2d19
Nadir Seghir <nadir.seghir@gmail.com>
2024-12-16 18:38:32 +0100

    Add support for Checkout block and Store API

--- js/rcfwc.js
+++ js/rcfwc.js
@@ -3,6 +3,8 @@ jQuery( document ).ready(function() {
     jQuery( document.body ).on( 'update_checkout updated_checkout applied_coupon_in_checkout removed_coupon_in_checkout checkout_error', function() {
         if(jQuery('.g-recaptcha').length > 0) {
             if (typeof grecaptcha !== "undefined" && typeof grecaptcha.reset === "function") {
+                console.log("Hello");
+                console.log(grecaptcha);
                 var count = 0;
                 jQuery(".g-recaptcha").each(function () {
                     grecaptcha.reset(count);
@@ -12,3 +14,20 @@ jQuery( document ).ready(function() {
         }
     });
 });
+
+if ( wp && wp.data ) {
+    var unsubscribe = wp.data.subscribe(function() {
+        const recaptcha = document.querySelector('.g-recaptcha');
+        if(recaptcha) {
+            grecaptcha.render(recaptcha, {
+                sitekey: recaptcha.dataset.sitekey,
+                callback: function(data) {
+                    wp.data.dispatch('wc/store/checkout').__internalSetExtensionData('rcfwc', {
+                        token: data
+                    })
+                }
+            });
+        }
+        unsubscribe();
+    }, 'wc/store/cart');
+}
